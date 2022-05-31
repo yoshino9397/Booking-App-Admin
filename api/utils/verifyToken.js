aa6bced5bae5c5804d08bab89dc718c30fdc7f9e
yoshino9397 <yosino8512@gmail.com>
2022-05-30 21:04:33 -0700

    fix some

--- api/utils/verifyToken.js
+++ api/utils/verifyToken.js
@@ -1,7 +1,7 @@
-import jwt from "jsonwebtoken";
-import { createError } from "../utils/error.js";
+const jwt = require("jsonwebtoken");
+const { createError } = require("../utils/error.js");
 
-export const verifyToken = (req, res, next) => {
+const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token;
   if (!token) {
     return next(createError(401, "You are not authenticated!"));
@@ -13,7 +13,7 @@ export const verifyToken = (req, res, next) => {
   });
 };
 
-export const verifyUser = (req, res, next) => {
+const verifyUser = (req, res, next) => {
   verifyToken(req, res, next, () => {
     if (req.user.id === req.params.id || req.user.isAdmin) {
       next();
@@ -23,7 +23,7 @@ export const verifyUser = (req, res, next) => {
   });
 };
 
-export const verifyAdmin = (req, res, next) => {
+const verifyAdmin = (req, res, next) => {
   verifyToken(req, res, next, () => {
     if (req.user.isAdmin) {
       next();
@@ -32,3 +32,5 @@ export const verifyAdmin = (req, res, next) => {
     }
   });
 };
+
+module.exports = { verifyToken, verifyAdmin, verifyUser };
